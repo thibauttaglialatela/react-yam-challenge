a34f6591abec66b328faad628af03488c33c22b0
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-04-08 17:22:41 +0200

    feat: create page rules

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from "./components/Navbar.jsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import YamRules from "./pages/YamRules.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
 <Navbar />
         <Routes>
             <Route path={"/"} element={<Home />} />
+            <Route path={"/rules"} element={<YamRules />} />
         </Routes>
     </>
   )
