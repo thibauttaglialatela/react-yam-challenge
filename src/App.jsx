76172a19950a72e85b606ca2046f378d50db98f5
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-04-08 17:14:15 +0200

    feat: create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Navbar from "./components/Navbar.jsx";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
 function App() {
 
   return (
     <>
 <Navbar />
+        <Routes>
+            <Route path={"/"} element={<Home />} />
+        </Routes>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+
+    return (
+        <>
+        <h1>Jouons au YAM</h1>
+            <button>Cliquez sur le bouton pour jouer</button>
+        </>
+    )
+}
+
+export default Home;
